fe2e7706c1c82ca920d2e9bab5b4adf763e14212
ololomob2 <opozdanets@gmail.com>
2017-04-16 15:40:19 +0400

    count inversions for 2 cases when pivot is the most left element and when pivot is the most right element

--- count_inversions/count_inversions.py
+++ count_inversions/count_inversions.py
@@ -4,12 +4,15 @@ with open('QuickSort.txt', 'r') as infile:
     for line in infile:
         nbrs.append(int(line.strip()))
 
+#test array
 tst = [3,2,8,5,1,4,7,6]
 
 def quick_sort(a,l,r):
+    #Key subroutine for quick sort where most left element is chosen as a pivot
     p = a[l]
     i = l + 1
     comps = 0
+    #Basic case to exit recursion
     if r == l + 1:
         comps += 1
         if a[l] > a[r]:
@@ -22,16 +25,20 @@ def quick_sort(a,l,r):
                 a[i], a[j] = a[j], a[i]
                 i += 1
         a[l], a[i-1] = a[i-1], a[l]
+        #Recursion call for the part less then the pivot
         if l < i-2:
             comps += quick_sort(a,l,i-2)
+        # Recursion call for the part bigger then the pivot
         if i < r:
             comps += quick_sort(a,i,r)
         return comps
 
 def quick_sortb(a,l,r):
+    # Key subroutine for quick sort where most right element is chosen as a pivot
     p = a[r]
     i = l
     comps = 0
+    # Basic case to exit recursion
     if r == l + 1:
         comps += 1
         if a[l] > a[r]:
@@ -44,17 +51,12 @@ def quick_sortb(a,l,r):
                 a[i], a[j] = a[j], a[i]
                 i += 1
         a[r], a[i] = a[i], a[r]
+        # Recursion call for the part less then the pivot
         if l < i-1:
             comps += quick_sortb(a,l,i-1)
+        # Recursion call for the part bigger then the pivot
         if i+1 < r:
             comps += quick_sortb(a,i+1,r)
         return comps
 
-#print(nbrs[:10])
-print(tst)
-
-print(quick_sortb(tst,0,len(tst)-1))
 print(quick_sortb(nbrs,0,len(nbrs)-1))
-print(tst)
-
-print(nbrs[:20])
