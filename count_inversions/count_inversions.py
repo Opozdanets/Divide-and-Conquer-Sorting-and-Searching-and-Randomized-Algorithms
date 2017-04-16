c87472cd0acd64a2d6cdfba2b0b2dadc7bf51644
ololomob2 <opozdanets@gmail.com>
2017-04-16 16:06:03 +0400

    3 cases with left, right and median pivots

--- count_inversions/count_inversions.py
+++ count_inversions/count_inversions.py
@@ -59,4 +59,55 @@ def quick_sortb(a,l,r):
             comps += quick_sortb(a,i+1,r)
         return comps
 
-print(quick_sortb(nbrs,0,len(nbrs)-1))
+def quick_sortm(a,l,r):
+    # Key subroutine for quick sort where most right element is chosen as a pivot
+    comps = 0
+    # Basic case to exit recursion
+    if r == l + 1:
+        comps += 1
+        if a[l] > a[r]:
+            a[l], a[r] = a[r], a[l]
+        return comps
+    else:
+        #Choosing "median" elenet as a pivot
+        q = l + ((r - l) // 2)
+        if (a[l] < a[q] and a[q] < a[r]) or (a[l] > a[q] and a[q] > a[r]):
+            p = a[q]
+            a[l], a[q] = a[q], a[l]
+        elif (a[l] < a[q] and a[r] < a[l]) or (a[l] > a[q] and a[r] > a[l]):
+            p = a[l]
+        elif (a[l] < a[r] and a[r] < a[q]) or (a[l] > a[r] and a[r] > a[q]):
+            p = a[r]
+            a[l], a[r] = a[r], a[l]
+        i = l + 1
+        comps += r - l
+        for j in range(l+1,r+1):
+            if a[j] < p:
+                a[i], a[j] = a[j], a[i]
+                i += 1
+        a[l], a[i-1] = a[i-1], a[l]
+        # Recursion call for the part less then the pivot
+        if l < i-2:
+            comps += quick_sortm(a,l,i-2)
+        # Recursion call for the part bigger then the pivot
+        if i < r:
+            comps += quick_sortm(a,i,r)
+        return comps
+
+print('Num of invers with a left pivot =', quick_sort(nbrs,0,len(nbrs)-1))
+
+#Reload initial array
+nbrs.clear()
+with open('QuickSort.txt', 'r') as infile:
+    for line in infile:
+        nbrs.append(int(line.strip()))
+
+print('Num of invers with a right pivot =', quick_sortb(nbrs,0,len(nbrs)-1))
+
+#Reload initial array
+nbrs.clear()
+with open('QuickSort.txt', 'r') as infile:
+    for line in infile:
+        nbrs.append(int(line.strip()))
+
+print('Num of invers with a median pivot =', quick_sortm(nbrs,0,len(nbrs)-1))
